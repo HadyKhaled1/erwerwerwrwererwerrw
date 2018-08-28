0f003a3627c865741ad79f8659dbce1577306eb7
HadyKhaled1 <42691592+HadyKhaled1@users.noreply.github.com>
2018-08-28 18:06:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,15 +4,16 @@ client.on('message', ra3d => {
 var prefix = "$";
                         let args = ra3d.content.split(" ").slice(1).join(" ")
 if(ra3d.content.startsWith(prefix + 'cc')) {
-    if(!args) return ra3d.channel.send('يرجا عليك كتابه رقم الالوان');
-             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('**انت لا تمتلك صلاحيه MANAGE_ROLES**'); 
-              ra3d.channel.send(:white_check_mark: |تم صنع ${args} الوان);
+    if(!args) return ra3d.channel.send('`يرجا عليك كتابه رقم الالوان`');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**انت لا تمتلك صلاحيه MANAGE_ROLES**'); 
+              ra3d.channel.send(`**✅ |تم صنع __${args}__ الوان**`);
                   setInterval(function(){})
                     let count = 0;
                     let ecount = 0;
-          for(let x = 1; x <${parseInt(args)+1}`; x++){
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
             ra3d.guild.createRole({name:x,
               color: 'RANDOM'})
               }
             }
+       });
 client.login(process.env.BOT_TOKEN);
