97f9cb33fd7e0895e5d77ade911148bee31794ec
HadyKhaled1 <42691592+HadyKhaled1@users.noreply.github.com>
2018-08-28 09:46:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -332,7 +332,7 @@ client.on('message', message => {
 });
 
 const developers = ["442670807240671252"]
-const adminprefixe = "!";
+const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
